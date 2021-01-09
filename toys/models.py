bb8bc68ec93d8396cfd35af22f1732d0f657877e
Alijonov <abdurasulalijonov98@mail.ru>
2021-01-09 15:09:03 +0500

    create new migrate

--- toys/models.py
+++ toys/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    firtname = models.CharField(max_length=25)
+    lastname = models.CharField(max_length=25)
+    age = models.IntegerField()
