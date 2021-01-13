14355eebb3b02e913c84319a4ea6aa36894507da
Alijonov <abdurasulalijonov98@mail.ru>
2021-01-13 09:34:52 +0500

    create new modul

--- toys/models.py
+++ toys/models.py
@@ -1,7 +1,37 @@
+from contextlib import nullcontext
+
 from django.db import models
 
 # Create your models here.
+class addres(models.Model):
+    street = models.CharField(max_length=25)
+    city = models.CharField(max_length=25,null=True,blank=True)
+    zip_code = models.CharField(max_length=25,null=True,blank=True)
+    country = models.CharField(max_length=25,null=True,blank=True)
+
+
+
 class user(models.Model):
     firtname = models.CharField(max_length=25)
-    lastname = models.CharField(max_length=25)
-    age = models.IntegerField()
+    lastname = models.CharField(max_length=25,null=True,blank=True)
+    email = models.EmailField(max_length=25,null=True,blank=True)
+    phone = models.CharField(max_length=25,null=True,blank=True)
+    age = models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.firtname
+
+
+    Addres = models.OneToOneField(addres,on_delete=models.PROTECT,null=True,blank=True)
+class tag(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.TextField(null=True,blank=True)
+
+class toy(models.Model):
+    name = models.CharField(max_length=25)
+    User = models.ForeignKey(user,related_name='toys',on_delete=models.CASCADE,null=True,blank=True)
+    description = models.TextField(null=True,blank=True)
+    tags = models.ManyToManyField(tag, related_name='toys')
+    created_at = models.TimeField(auto_now=True)
+    upteted_at = models.TimeField(auto_now=True)
+
--- toys/views.py
+++ toys/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def dashboard(request):
-    return render(request,'toys/dashboard.html')
+    users = user.objects.all()
+    toys = toy.objects.all()
+    return render(request,'toys/dashboard.html',{'users': users,'toys':toys})
